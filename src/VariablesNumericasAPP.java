5baaaab91d589516039b2a8d7e741ce1b50cc713
Abel-Git2103 <abel_jornet@hotmail.com>
2022-01-12 10:56:05 +0100

    PRIMER COMMIT

---
+++ src/VariablesNumericasAPP.java
@@ -0,0 +1,16 @@
+
+public class VariablesNumericasAPP {
+
+	public static void main(String[] args) {
+		
+		int variableA = 19, variableB = 20;
+		
+		if(variableA > variableB) {
+			System.out.println("La variable A es mayor que la variable B");
+		}else if(variableA < variableB) {
+			System.out.println("La variable A es menor que la variable B");
+		}else {
+			System.out.println("La variable A es igual a la variable B");
+		}
+	}
+}
